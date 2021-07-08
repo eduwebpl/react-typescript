68599c4fa3dd2089c6bfa039342fc9a90d7da2a1
Adam Gospodarczyk <adam@overment.com>
2021-07-08 22:44:52 +0200

    08

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-import React, { Component, FC, ReactElement } from 'react';
-import PropTypes from 'prop-types';
-
-const MoviePropTypes = {
-    title: PropTypes.string.isRequired,
-    duration: PropTypes.number,
-    platforms: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-};
-
-interface IMovieProps {
-    title?: string;
-    duration?: number;
-    platforms: string[];
-}
-
-const Movie: FC<IMovieProps> = (props) => {
-    return null;
-};
-Movie.propTypes = MoviePropTypes;
-
-class Movie2 extends Component<IMovieProps> {
-    static propTypes = MoviePropTypes;
-
-    render() {
-        return null;
-    }
-}
+import React, { ReactElement } from 'react';
 
 function App(): ReactElement {
-    return (
-        <div className="App">
-            <Movie2 platforms={['netflix']} />
-        </div>
-    );
+    return <div className="App">...</div>;
 }
 
 export default App;
