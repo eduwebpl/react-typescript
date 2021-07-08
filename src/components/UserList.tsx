1a5ed36b736fbb7c1820a768c204ba150bcd0d05
Adam Gospodarczyk <adam@overment.com>
2021-07-08 22:15:05 +0200

    06-finish

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-interface IUserProps {
+const defaultProps = {
+    active: false,
+};
+
+type UserProps = {
     /** Full name of a user */ // TSDoc
     name: string;
-    email: string;
-    roles?: string[];
-}
+    active?: boolean;
+} & typeof defaultProps;
 
-const User = ({ name, email, roles = [] }: IUserProps) => {
-    return (
-        <li>
-            {name} ({email}) [{roles.map((role) => role)}]
-        </li>
-    );
+const User = ({ name, active = false }: UserProps) => {
+    return <li className={`${active ? 'user--is-active' : ''}`}>{name}</li>;
 };
+
+User.defaultProps = defaultProps;
+
+type User2Props = typeof User2.defaultProps & {
+    name: string;
+    active: boolean;
+};
+
+class User2 extends Component<User2Props> {
+    static defaultProps = { active: false };
+
+    render() {
+        return <li className={`${this.props.active ? 'user--is-active' : ''}`}>{this.props.name}</li>;
+    }
+}
 
 const UserList = () => {
     return (
         <ul>
-            <User name={'Adam'} email={'adam@overment.com'} />
+            <User name={'Adam'} />
         </ul>
     );
 };
