2e6d46125f5e5d35a6c5393608ab7bbdba7cf4be
Adam Gospodarczyk <adam@overment.com>
2021-07-09 17:05:38 +0200

    12

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,4 @@
-import React, { Component, ReactElement } from 'react';
-
-interface IMessageProps {
-    defaultTimes: number;
-}
-
-interface IMessageState {
-    times: number;
-}
-
-class Message extends Component<IMessageProps, IMessageState> {
-    constructor(props: IMessageProps) {
-        super(props);
-    }
-
-    static defaultProps = {
-        defaultTimes: 3,
-    };
-
-    readonly state: IMessageState = {
-        times: 0,
-    };
-
-    componentDidUpdate(prevProps: IMessageProps, prevState: IMessageState) {
-        ///
-    }
-
-    render() {
-        return (
-            <>
-                <h1>I 💛 you x{this.state.times}</h1>
-                <button onClick={() => this.setState(({ times }) => ({ times: times + 1 }))}>
-                    I need more love 💔
-                    {this.props.defaultTimes}
-                </button>
-            </>
-        );
-    }
-}
+import React, { ReactElement } from 'react';
 
 function App(): ReactElement {
     return <div></div>;
