40c1a32c45f3b2ffc3b5eb651e24b4e42867f017
Adam Gospodarczyk <adam@overment.com>
2021-07-09 19:43:18 +0200

    14-finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
+
+const Counter = () => {
+    const [counter, setCounter] = useState(0);
+
+    const moreLove = (e: React.MouseEvent) => {
+        setCounter(counter + 1);
+    };
+
+    return (
+        <>
+            <div>I 💛 {counter} times!</div>
+            <button onClick={moreLove}>Give me more!</button>
+        </>
+    );
+};
 
 function App(): ReactElement {
-    return <div>...</div>;
+    return (
+        <div>
+            <Counter />
+        </div>
+    );
 }
 
 export default App;
