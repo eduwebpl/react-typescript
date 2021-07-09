5a1834fdf84d7124b2f31516e5d00ebe7757d7a3
Adam Gospodarczyk <adam@overment.com>
2021-07-09 16:51:36 +0200

    11-finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
-import React, { FC, ReactElement } from 'react';
+import React, { Component, ReactElement } from 'react';
 
-type MessageProps = { text: string };
+interface IMessageProps {
+    defaultTimes: number;
+}
 
-const Message = ({ text }: MessageProps) => {
-    return <>{text}</>;
-};
+interface IMessageState {
+    times: number;
+}
+
+class Message extends Component<IMessageProps, IMessageState> {
+    constructor(props: IMessageProps) {
+        super(props);
+    }
+
+    static defaultProps = {
+        defaultTimes: 3,
+    };
+
+    readonly state: IMessageState = {
+        times: 0,
+    };
+
+    componentDidUpdate(prevProps: IMessageProps, prevState: IMessageState) {
+        ///
+    }
+
+    render() {
+        return (
+            <>
+                <h1>I 💛 you x{this.state.times}</h1>
+                <button onClick={() => this.setState(({ times }) => ({ times: times + 1 }))}>
+                    I need more love 💔
+                    {this.props.defaultTimes}
+                </button>
+            </>
+        );
+    }
+}
 
 function App(): ReactElement {
-    return (
-        <div>
-            <Message text={'Hello!'} />
-        </div>
-    );
+    return <div></div>;
 }
 
 export default App;
