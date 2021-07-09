81de1e3f461cae592815e0da25d7b891d9af33fb
Adam Gospodarczyk <adam@overment.com>
2021-07-09 15:51:52 +0200

    10

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,7 @@
 import React, { Component, MouseEvent, ReactElement } from 'react';
 
-interface IMouseState {
-    x: number;
-    y: number;
-}
-
-interface IMouseProps {
-    render(state: IMouseState): ReactElement;
-}
-
-interface ICatProps {
-    mouse: IMouseState;
-}
-
-const Cat = ({ mouse }: ICatProps) => {
-    return (
-        <img src={'https://placekitten.com/100/100'} style={{ position: 'absolute', left: mouse.x, top: mouse.y }} />
-    );
-};
-
-class Mouse extends Component<IMouseProps, IMouseState> {
-    constructor(props: IMouseProps) {
-        super(props);
-        this.state = { x: 0, y: 0 };
-    }
-
-    handleMouseMove = (event: MouseEvent) => {
-        this.setState({
-            x: event.clientX,
-            y: event.clientY,
-        });
-    };
-
-    render() {
-        return (
-            <div style={{ height: '100vh', width: '100vw' }} onMouseMove={this.handleMouseMove}>
-                {this.props.render(this.state)}
-            </div>
-        );
-    }
-}
-
 function App(): ReactElement {
-    return (
-        <div>
-            <Mouse render={(mouse) => <Cat mouse={mouse} />} />
-        </div>
-    );
+    return <div>...</div>;
 }
 
 export default App;
