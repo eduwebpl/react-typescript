034b595a53b6025319e866831362172da5c827c1
Adam Gospodarczyk <adam@overment.com>
2021-07-21 19:26:05 +0200

    19-finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const useToggle = (init: boolean) => {
+    const [value, setValue] = useState(init);
+    const toggleValue = () => setValue(!value);
+
+    return [value, toggleValue] as const;
+};
 
 function App() {
-    return <>...</>;
+    const [isActive, toggleActive] = useToggle(false);
+    return (
+        <div className="App">
+            {isActive && <>I&apos;m here!</>}
+            {isActive}
+            <button onClick={toggleActive}>Switch!</button>
+        </div>
+    );
 }
 
 export default App;
