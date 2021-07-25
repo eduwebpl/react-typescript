194ca423f6df7ea166546024a373b74756dfd53f
Adam Gospodarczyk <adam@overment.com>
2021-07-25 15:13:02 +0200

    21

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
 
-//  AnimationEvent, ChangeEvent,
-//  ClipboardEvent, CompositionEvent,
-//  DragEvent, FocusEvent, FormEvent,
-//  KeyboardEvent, MouseEvent, PointerEvent,
-//  TouchEvent, TransitionEvent, WheelEvent.
-//  As well as SyntheticEvent, for all other events.
-
 function App() {
-    const action: React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement> = (e) => {
-        const target = e.currentTarget;
-        if ('href' in target) {
-            console.log(target.href);
-        }
-    };
-
-    return <button onClick={action}>...</button>;
+    return <>...</>;
 }
 
 export default App;
