354f5fb94cbf9fb5db6ab1fae7c8244d197be3f3
Adam Gospodarczyk <adam@overment.com>
2021-07-27 14:36:37 +0200

    21-finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
+
+interface IFields {
+    email: HTMLInputElement;
+    message: HTMLInputElement;
+}
 
 function App() {
-    return <>...</>;
+    const submit = (event: FormEvent<HTMLFormElement>) => {
+        const target = event.target as typeof event.target & IFields;
+        console.log(target.email);
+    };
+
+    const validateEmail: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        // validateEmail
+    };
+
+    return (
+        <>
+            <form name="signup" onSubmit={submit}>
+                <input onChange={validateEmail} type="email" name={'email'} />
+                <input type="text" name={'name'} />
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    );
 }
 
 export default App;
